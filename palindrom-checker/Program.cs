ee25242cb967d8717441daf04eaa53e9b01f2a4b
fiorenzaa <fiorenza.laila24@gmail.com>
2025-12-04 15:27:15 +0700

    Membuat program dan running

---
+++ palindrom-checker/Program.cs
@@ -0,0 +1,17 @@
+﻿static bool IsPalindrome(string? text)
+{
+  if (string.IsNullOrWhiteSpace(text))
+    return false;
+
+  string reversed = "";
+
+  for (int i = text.Length - 1; i >= 0; i--)
+    reversed += text[i];
+
+  return text == reversed;
+}
+
+Console.Write("Masukkan kata: ");
+string? input = Console.ReadLine();
+
+Console.WriteLine(IsPalindrome(input) ? "Palindrome" : "Bukan Palindrome");
